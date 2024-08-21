ffe7aec9a2e1928c45c68da9ede24c23ccd3eb23
AJ <ashokjeevan@gmail.com>
2024-08-21 00:35:08 -0400

    Delete elasticache-iam directory

--- elasticache-iam/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-dev-august152024"
-    key = "dev/tf-elasticache-iam"
-    region = "ca-central-1"
-    dynamodb_table = "terraform-state-lock-dev"
-  }
-}
